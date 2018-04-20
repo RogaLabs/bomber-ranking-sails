4e168a072b42e3eb5ad1e50f37022926d94b8cb9
Siderlan Santos <sider@Siderlans-MacBook-Pro.local>
2018-04-19 22:04:11 -0300

    Remove stripe integration

--- api/controllers/entrance/confirm-email.js
+++ api/controllers/entrance/confirm-email.js
@@ -1,53 +1,43 @@
 module.exports = {
-
-
   friendlyName: 'Confirm email',
 
-
-  description:
-`Confirm a new user's email address, or an existing user's request for an email address change,
+  description: `Confirm a new user's email address, or an existing user's request for an email address change,
 then redirect to either a special landing page (for newly-signed up users), or the account page
 (for existing users who just changed their email address).`,
 
-
   inputs: {
-
     token: {
       description: 'The confirmation token from the email.',
       example: '4-32fad81jdaf$329'
     }
-
   },
 
-
   exits: {
-
     success: {
       description: 'Email address confirmed and requesting user logged in.'
     },
 
     redirect: {
-      description: 'Email address confirmed and requesting user logged in.  Since this looks like a browser, redirecting...',
+      description:
+        'Email address confirmed and requesting user logged in.  Since this looks like a browser, redirecting...',
       responseType: 'redirect'
     },
 
     invalidOrExpiredToken: {
       responseType: 'expired',
-      description: 'The provided token is expired, invalid, or already used up.',
+      description: 'The provided token is expired, invalid, or already used up.'
     },
 
     emailAddressNoLongerAvailable: {
       statusCode: 409,
       viewTemplatePath: '500',
       description: 'The email address is no longer available.',
-      extendedDescription: 'This is an edge case that is not always anticipated by websites and APIs.  Since it is pretty rare, the 500 server error page is used as a simple catch-all.  If this becomes important in the future, this could easily be expanded into a custom error page or resolution flow.  But for context: this behavior of showing the 500 server error page mimics how popular apps like Slack behave under the same circumstances.',
+      extendedDescription:
+        'This is an edge case that is not always anticipated by websites and APIs.  Since it is pretty rare, the 500 server error page is used as a simple catch-all.  If this becomes important in the future, this could easily be expanded into a custom error page or resolution flow.  But for context: this behavior of showing the 500 server error page mimics how popular apps like Slack behave under the same circumstances.'
     }
-
   },
 
-
-  fn: async function (inputs, exits) {
-
+  fn: async function(inputs, exits) {
     // If no token was provided, this is automatically invalid.
     if (!inputs.token) {
       throw 'invalidOrExpiredToken';
@@ -81,13 +71,16 @@ then redirect to either a special landing page (for newly-signed up users), or t
       } else {
         throw { redirect: '/email/confirmed' };
       }
-
     } else if (user.emailStatus === 'changeRequested') {
       //  ┌─┐┌─┐┌┐┌┌─┐┬┬─┐┌┬┐┬┌┐┌┌─┐  ╔═╗╦ ╦╔═╗╔╗╔╔═╗╔═╗╔╦╗  ┌─┐┌┬┐┌─┐┬┬
       //  │  │ ││││├┤ │├┬┘││││││││ ┬  ║  ╠═╣╠═╣║║║║ ╦║╣  ║║  ├┤ │││├─┤││
       //  └─┘└─┘┘└┘└  ┴┴└─┴ ┴┴┘└┘└─┘  ╚═╝╩ ╩╩ ╩╝╚╝╚═╝╚═╝═╩╝  └─┘┴ ┴┴ ┴┴┴─┘
-      if (!user.emailChangeCandidate){
-        throw new Error(`Consistency violation: Could not update Stripe customer because this user record's emailChangeCandidate ("${user.emailChangeCandidate}") is missing.  (This should never happen.)`);
+      if (!user.emailChangeCandidate) {
+        throw new Error(
+          `Consistency violation: Could not update Stripe customer because this user record's emailChangeCandidate ("${
+            user.emailChangeCandidate
+          }") is missing.  (This should never happen.)`
+        );
       }
 
       // Last line of defense: since email change candidates are not protected
@@ -95,28 +88,8 @@ then redirect to either a special landing page (for newly-signed up users), or t
       // sure no one else managed to grab this email in the mean time since we
       // last checked its availability. (This is a relatively rare edge case--
       // see exit description.)
-      if (await User.count({ emailAddress: user.emailChangeCandidate }) > 0) {
+      if ((await User.count({ emailAddress: user.emailChangeCandidate })) > 0) {
         throw 'emailAddressNoLongerAvailable';
-      }
-
-      // If billing features are enabled, also update the billing email for this
-      // user's linked customer entry in the Stripe API to make sure they receive
-      // email receipts.
-      // > Note: If there was not already a Stripe customer entry for this user,
-      // > then one will be set up implicitly, so we'll need to persist it to our
-      // > database.  (This could happen if Stripe credentials were not configured
-      // > at the time this user was originally created.)
-      if(sails.config.custom.enableBillingFeatures) {
-        let didNotAlreadyHaveCustomerId = (! user.stripeCustomerId);
-        let stripeCustomerId = await sails.helpers.stripe.saveBillingInfo.with({
-          stripeCustomerId: user.stripeCustomerId,
-          emailAddress: user.emailChangeCandidate
-        });
-        if (didNotAlreadyHaveCustomerId){
-          await User.update({ id: user.id }).set({
-            stripeCustomerId
-          });
-        }
       }
 
       // Finally update the user in the database, store their id in the session
@@ -127,7 +100,7 @@ then redirect to either a special landing page (for newly-signed up users), or t
         emailProofToken: '',
         emailProofTokenExpiresAt: 0,
         emailAddress: user.emailChangeCandidate,
-        emailChangeCandidate: '',
+        emailChangeCandidate: ''
       });
       this.req.session.userId = user.id;
       if (this.req.wantsJSON) {
@@ -135,12 +108,14 @@ then redirect to either a special landing page (for newly-signed up users), or t
       } else {
         throw { redirect: '/account' };
       }
-
     } else {
-      throw new Error(`Consistency violation: User ${user.id} has an email proof token, but somehow also has an emailStatus of "${user.emailStatus}"!  (This should never happen.)`);
+      throw new Error(
+        `Consistency violation: User ${
+          user.id
+        } has an email proof token, but somehow also has an emailStatus of "${
+          user.emailStatus
+        }"!  (This should never happen.)`
+      );
     }
-
   }
-
-
 };
--- api/controllers/entrance/signup.js
+++ api/controllers/entrance/signup.js
@@ -1,14 +1,9 @@
 module.exports = {
-
-
   friendlyName: 'Signup',
-
 
   description: 'Sign up for a new user account.',
 
-
-  extendedDescription:
-`This creates a new user record in the database, signs in the requesting user agent
+  extendedDescription: `This creates a new user record in the database, signs in the requesting user agent
 by modifying its [session](https://sailsjs.com/documentation/concepts/sessions), and
 (if emailing with Mailgun is enabled) sends an account verification email.
 
@@ -16,15 +11,13 @@ If a verification email is sent, the new user's account is put in an "unconfirme
 until they confirm they are using a legitimate email address (by clicking the link in
 the account verification message.)`,
 
-
   inputs: {
-
     emailAddress: {
       required: true,
       type: 'string',
       isEmail: true,
       description: 'The email address for the new account, e.g. m@example.com.',
-      extendedDescription: 'Must be a valid email address.',
+      extendedDescription: 'Must be a valid email address.'
     },
 
     password: {
@@ -35,63 +28,58 @@ the account verification message.)`,
       description: 'The unencrypted password to use for the new account.'
     },
 
-    fullName:  {
+    fullName: {
       required: true,
       type: 'string',
       example: 'Frida Kahlo de Rivera',
-      description: 'The user\'s full name.',
+      description: 'The user\'s full name.'
     }
-
   },
 
-
   exits: {
-
     invalid: {
       responseType: 'badRequest',
-      description: 'The provided fullName, password and/or email address are invalid.',
-      extendedDescription: 'If this request was sent from a graphical user interface, the request '+
-      'parameters should have been validated/coerced _before_ they were sent.'
+      description:
+        'The provided fullName, password and/or email address are invalid.',
+      extendedDescription:
+        'If this request was sent from a graphical user interface, the request ' +
+        'parameters should have been validated/coerced _before_ they were sent.'
     },
 
     emailAlreadyInUse: {
       statusCode: 409,
-      description: 'The provided email address is already in use.',
-    },
-
+      description: 'The provided email address is already in use.'
+    }
   },
 
-
-  fn: async function (inputs, exits) {
-
+  fn: async function(inputs, exits) {
     var newEmailAddress = inputs.emailAddress.toLowerCase();
 
     // Build up data for the new user record and save it to the database.
     // (Also use `fetch` to retrieve the new ID so that we can use it below.)
-    var newUserRecord = await User.create(Object.assign({
-      emailAddress: newEmailAddress,
-      password: await sails.helpers.passwords.hashPassword(inputs.password),
-      fullName: inputs.fullName,
-      tosAcceptedByIp: this.req.ip
-    }, sails.config.custom.verifyEmailAddresses? {
-      emailProofToken: await sails.helpers.strings.random('url-friendly'),
-      emailProofTokenExpiresAt: Date.now() + sails.config.custom.emailProofTokenTTL,
-      emailStatus: 'unconfirmed'
-    }:{}))
-    .intercept('E_UNIQUE', 'emailAlreadyInUse')
-    .intercept({name: 'UsageError'}, 'invalid')
-    .fetch();
-
-    // If billing feaures are enabled, save a new customer entry in the Stripe API.
-    // Then persist the Stripe customer id in the database.
-    if (sails.config.custom.enableBillingFeatures) {
-      let stripeCustomerId = await sails.helpers.stripe.saveBillingInfo.with({
-        emailAddress: newEmailAddress
-      });
-      await User.update(newUserRecord.id).set({
-        stripeCustomerId
-      });
-    }
+    var newUserRecord = await User.create(
+      Object.assign(
+        {
+          emailAddress: newEmailAddress,
+          password: await sails.helpers.passwords.hashPassword(inputs.password),
+          fullName: inputs.fullName,
+          tosAcceptedByIp: this.req.ip
+        },
+        sails.config.custom.verifyEmailAddresses
+          ? {
+            emailProofToken: await sails.helpers.strings.random(
+                'url-friendly'
+            ),
+            emailProofTokenExpiresAt:
+                Date.now() + sails.config.custom.emailProofTokenTTL,
+            emailStatus: 'unconfirmed'
+          }
+          : {}
+      )
+    )
+      .intercept('E_UNIQUE', 'emailAlreadyInUse')
+      .intercept({ name: 'UsageError' }, 'invalid')
+      .fetch();
 
     // Store the user's new id in their session.
     this.req.session.userId = newUserRecord.id;
@@ -108,12 +96,12 @@ the account verification message.)`,
         }
       });
     } else {
-      sails.log.info('Skipping new account email verification... (since `verifyEmailAddresses` is disabled)');
+      sails.log.info(
+        'Skipping new account email verification... (since `verifyEmailAddresses` is disabled)'
+      );
     }
 
     // Since everything went ok, send our 200 response.
     return exits.success();
-
   }
-
 };
